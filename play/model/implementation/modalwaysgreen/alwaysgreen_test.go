6368f6272441fb20482fed9716d3adcd3797e577
uszebr <uszebr@gmail.com>
2024-05-21 10:49:07 -0400

    refactor model tests

--- play/model/implementation/modalwaysgreen/alwaysgreen_test.go
+++ play/model/implementation/modalwaysgreen/alwaysgreen_test.go
@@ -1,6 +1,7 @@
 package modalwaysgreen
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/uszebr/thegamem/play/signal"
@@ -24,14 +25,16 @@ func TestModelAlwaysGreen(t *testing.T) {
 		//	{myHistory: []signal.Signal{signal.Red}, opHistory: []signal.Signal{}, result: signal.Red}, // fail to check
 	}
 
-	for index, test := range tests {
-		model := ModAlwaysGreen{}.GetModel()
-		actualResult := model.CalculateSignal(test.myHistory, test.opHistory, aproximateInteractions)
+	for index, testL := range tests {
+		test := testL // for multithreading safe before 1.22
+		t.Run(fmt.Sprintf("Test: %v", index), func(t *testing.T) {
+			model := ModAlwaysGreen{}.GetModel()
+			actualResult := model.CalculateSignal(test.myHistory, test.opHistory, aproximateInteractions)
 
-		if actualResult != test.result {
-			t.Errorf("i: %d actual result: %v not as expected: %v; ", index, actualResult, test.result)
-		}
-
+			if actualResult != test.result {
+				t.Errorf("i: %d actual result: %v not as expected: %v; ", index, actualResult, test.result)
+			}
+		})
 	}
 
 }
