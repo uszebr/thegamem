1c6195ce0b3b57fc37b2b17360294d9e28258bab
uszebr <uszebr@gmail.com>
2024-05-25 20:52:13 -0400

    refactor jwtservice; move userauth entity

--- internal/jwtservice/jwtservice.go
+++ internal/jwtservice/jwtservice.go
@@ -5,22 +5,22 @@ import (
 	"log/slog"
 
 	"github.com/dgrijalva/jwt-go"
-	"github.com/uszebr/thegamem/internal/authservice"
+	"github.com/uszebr/thegamem/internal/entity"
 	"github.com/uszebr/thegamem/internal/logger/loggerinit"
 	"github.com/uszebr/thegamem/internal/logger/logutil"
 )
 
 // example of decoding https://jwt.io/
-func GetUserFromJWT(tokenExt string) (authservice.UserAuth, error) {
+func GetUserFromJWT(tokenExt string) (entity.UserAuth, error) {
 	token, _, err := new(jwt.Parser).ParseUnverified(tokenExt, jwt.MapClaims{})
 	if err != nil {
 		slog.Error("Error parsing JWT", logutil.Err(err))
-		return authservice.UserAuth{}, fmt.Errorf("Error parsing JWT: %v", err)
+		return entity.UserAuth{}, fmt.Errorf("Error parsing JWT: %v", err)
 	}
 	claims, ok := token.Claims.(jwt.MapClaims)
 	if !ok {
 		loggerinit.GetLogger().Error("Invalid JWT claims")
-		return authservice.UserAuth{}, fmt.Errorf("Invalid JWT claims")
+		return entity.UserAuth{}, fmt.Errorf("Invalid JWT claims")
 	}
 
 	// Extracting data
@@ -30,7 +30,7 @@ func GetUserFromJWT(tokenExt string) (authservice.UserAuth, error) {
 	isAnonymous, isAnonymousOk := claims["is_anonymous"].(bool)
 	// Check if all required fields are present
 	if !emailOk || !subOk || !isAnonymousOk || !roleOk {
-		return authservice.UserAuth{}, fmt.Errorf("Missing required fields in JWT claims")
+		return entity.UserAuth{}, fmt.Errorf("Missing required fields in JWT claims")
 	}
-	return authservice.UserAuth{Email: email, Role: role, IsAnonymus: isAnonymous, UserId: sub}, nil
+	return entity.UserAuth{Email: email, Role: role, IsAnonymus: isAnonymous, UserId: sub}, nil
 }
