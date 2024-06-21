e2ff9213e2fe56641ccbf6630fc04879729059ae
uszebr <uszebr@gmail.com>
2024-06-21 01:04:58 -0400

    add func chartutil + test

--- internal/chart/chartutil/chartutil.go
+++ internal/chart/chartutil/chartutil.go
@@ -17,3 +17,12 @@ func SliceIntToString(nums []int) string {
 	sb.WriteString("]")
 	return sb.String()
 }
+
+// result [1, 2, 3, 4, 5, 6, 7, 8, 9]
+func SliceBorderToString(leftIncluded int, rightNotIncluded int) string {
+	var result []string
+	for i := leftIncluded; i < rightNotIncluded; i++ {
+		result = append(result, fmt.Sprintf("%d", i))
+	}
+	return "[" + strings.Join(result, ", ") + "]"
+}
